90d0b4f492183a6fd797c7ad7800cc29f2229d19
MacielFede <fmaci1324@gmail.com>
2022-12-02 15:56:30 -0300

    Update firstChallenge.js

--- firstChallenge.js
+++ firstChallenge.js
@@ -1,4 +1,5 @@
 function wrapping(gifts) {
+ //In this challenge I use the .map() method to return a new array with the gifts already wrapped
  return gifts.map(gift =>{
    const paper = '*'.repeat(gift.length + 2);
     gift =  paper + "\n*" + gift + "*\n" + paper;
